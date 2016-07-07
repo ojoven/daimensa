ba3b6b8eca1d8f6f3b57212a549f7a0fe3dcf080
ojoven <mikeltorresmail@gmail.com>
2016-07-07 13:37:57 +0200

    Add word frequency to word candidates

--- app/Models/LessonWord.php
+++ app/Models/LessonWord.php
@@ -17,6 +17,17 @@ class LessonWord extends Model {
 
         $candidates = array();
 
+        $wordFrequencies = json_decode(file_get_contents(base_path() . '/data/fr/jsons/wordFrequencies.json'), true);
+        $candidateWords = array();
+
+        foreach ($words as $word) {
+            if (array_key_exists($word['word'], $wordFrequencies)) {
+                array_push($candidateWords, $word);
+            }
+        }
+
+        asort($candidateWords);
+
         // To be optimized with the algorithm
         while (count($candidates) < $this->numWords && count($candidates) < count($words)) {
 
